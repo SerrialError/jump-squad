fc54920ceef24dd1e624d697cbff6b6eb5b2feba
Serrial Error <serrialerror@outlook.com>
2024-08-24 22:06:06 -0700

    Fixed Sidebar

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,35 +1,34 @@
-import * as React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Link,
   useLocation,
 } from "react-router-dom";
 import { styled, useTheme } from "@mui/material/styles";
-import { useAuth } from '../components/AuthContext';
 import Box from "@mui/material/Box";
-import MuiDrawer from "@mui/material/Drawer";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
 import MuiAppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import List from "@mui/material/List";
-import CssBaseline from "@mui/material/CssBaseline";
 import Typography from "@mui/material/Typography";
-import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
 import DashboardIcon from "@mui/icons-material/Dashboard";
-import InfoIcon from '@mui/icons-material/Info';
-import AnalyticsIcon from "@mui/icons-material/Analytics";
-import FolderIcon from '@mui/icons-material/Folder';
-import FlagIcon from '@mui/icons-material/Flag';
+import StatsIcon from "@mui/icons-material/InsertChart";
+import GoalsIcon from "@mui/icons-material/Flag";
+import DirectoryIcon from "@mui/icons-material/Business";
+import PublicProfileIcon from "@mui/icons-material/Person";
+import Divider from "@mui/material/Divider";
 import Dashboard from "./Dashboard";
 import Statistics from "./Statistics";
 import Account from "./Account";
@@ -39,80 +38,57 @@ import Signup from "./Signup";
 import Goals from "./Goals";
 import Directory from "./Directory";
 import PublicProfile from "./PublicProfile";
-import supabase from '../components/Supabase';
-import Avatar from '../components/Avatar';
+import supabase from "../components/Supabase";
+import Avatar from "../components/Avatar";
 
 const drawerWidth = 240;
 
-const openedMixin = (theme) => ({
-  width: drawerWidth,
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: "hidden",
-});
-
-const closedMixin = (theme) => ({
-  transition: theme.transitions.create("width", {
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(({ theme, open }) => ({
+  flexGrow: 1,
+  padding: theme.spacing(3),
+  transition: theme.transitions.create("margin", {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
-  overflowX: "hidden",
-  width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up("sm")]: {
-    width: `calc(${theme.spacing(8)} + 1px)`,
-  },
-});
-
-const DrawerHeader = styled("div")(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "flex-end",
-  padding: theme.spacing(0, 1),
-  ...theme.mixins.toolbar,
+  marginLeft: `-${drawerWidth}px`,
+  ...(open && {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  }),
 }));
 
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== "open",
-})(({ theme, open }) => ({
+const AppBar = styled(MuiAppBar)(({ theme, open }) => ({
   zIndex: theme.zIndex.drawer + 1,
   transition: theme.transitions.create(["width", "margin"], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
   ...(open && {
-    marginLeft: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
     transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
+      easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
   }),
 }));
 
-const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== "open",
-})(({ theme, open }) => ({
-  width: drawerWidth,
-  flexShrink: 0,
-  whiteSpace: "nowrap",
-  boxSizing: "border-box",
-  ...(open && {
-    ...openedMixin(theme),
-    "& .MuiDrawer-paper": openedMixin(theme),
-  }),
-  ...(!open && {
-    ...closedMixin(theme),
-    "& .MuiDrawer-paper": closedMixin(theme),
-  }),
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
 }));
 
 function Navbar() {
   const theme = useTheme();
-  const [open, setOpen] = useState(false);
   const location = useLocation();
   const [session, setSession] = useState(null);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
@@ -122,7 +98,6 @@ function Navbar() {
     supabase.auth.onAuthStateChange((_event, session) => {
       setSession(session);
     });
-
   }, []);
 
   const handleDrawerOpen = () => {
@@ -165,79 +140,111 @@ function Navbar() {
         <Toolbar>
           <IconButton
             color="inherit"
-            aria-label="open menu"
+            aria-label="open drawer"
             onClick={handleDrawerOpen}
             edge="start"
-            sx={{
-              marginRight: 5,
-              ...(open && { display: "none" }),
-            }}
+            sx={{ mr: 2, ...(open && { display: "none" }) }}
           >
             <MenuIcon />
           </IconButton>
-          <Box
-            display="flex"
-            justifyContent="space-between"
-            alignItems="center"
-            flexGrow={1}
-          >
-            <Typography variant="h6" noWrap component="div">
-              {getPageTitle()}
-            </Typography>
-            <IconButton
-              className="account-icon"
-              color="inherit"
-            >
-	      <Avatar />
-            </IconButton>
-          </Box>
+          <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+            {getPageTitle()}
+          </Typography>
+          <IconButton className="account-icon" color="inherit">
+            <Avatar />
+          </IconButton>
         </Toolbar>
       </AppBar>
-      <Drawer variant="permanent" open={open}>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
         <DrawerHeader>
-          <IconButton aria-label="open menu" onClick={handleDrawerClose}>
-            {theme.direction === "rtl" ? (
-              <ChevronRightIcon />
-            ) : (
-              <ChevronLeftIcon />
-            )}
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "ltr" ? <ChevronLeftIcon /> : <ChevronRightIcon />}
           </IconButton>
         </DrawerHeader>
         <Divider />
         <List>
-          {[
-            { text: "Dashboard", icon: <DashboardIcon /> },
-            { text: "Directory", icon: <FolderIcon /> },
-            { text: "Statistics", icon: <AnalyticsIcon /> },
-            { text: "Goals", icon: <FlagIcon /> },
-            { text: "About", icon: <InfoIcon /> },
-          ].map(({ text, icon }, index) => (
-            <ListItem key={text} disablePadding sx={{ display: "block" }}>
-              <ListItemButton
-                component={Link}
-                to={text === "Dashboard" ? "/" : `/${text}`}
-                sx={{
-                  minHeight: 48,
-                  justifyContent: open ? "initial" : "center",
-                  px: 2.5,
-                }}
-              >
-                <ListItemIcon
-                  sx={{
-                    minWidth: 0,
-                    mr: open ? 3 : "auto",
-                    justifyContent: "center",
-                  }}
-                >
-                  {icon}
-                </ListItemIcon>
-                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
-              </ListItemButton>
-            </ListItem>
-          ))}
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="/">
+              <ListItemIcon>
+                <DashboardIcon />
+              </ListItemIcon>
+              <ListItemText primary="Dashboard" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="/Statistics">
+              <ListItemIcon>
+                <StatsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Statistics" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="/Goals">
+              <ListItemIcon>
+                <GoalsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Goals" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="/Directory">
+              <ListItemIcon>
+                <DirectoryIcon />
+              </ListItemIcon>
+              <ListItemText primary="Directory" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="/PublicProfile">
+              <ListItemIcon>
+                <PublicProfileIcon />
+              </ListItemIcon>
+              <ListItemText primary="Public Profile" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+        <Divider />
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="/About">
+              <ListItemIcon>
+                <MailIcon />
+              </ListItemIcon>
+              <ListItemText primary="About" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="/Login">
+              <ListItemIcon>
+                <MailIcon />
+              </ListItemIcon>
+              <ListItemText primary="Login" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="/Signup">
+              <ListItemIcon>
+                <MailIcon />
+              </ListItemIcon>
+              <ListItemText primary="Signup" />
+            </ListItemButton>
+          </ListItem>
         </List>
       </Drawer>
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+      <Main open={open}>
         <DrawerHeader />
         <Routes>
           <Route path="/" element={<Dashboard />} />
@@ -250,7 +257,7 @@ function Navbar() {
           <Route path="/Directory" element={<Directory />} />
           <Route path="/PublicProfile" element={<PublicProfile />} />
         </Routes>
-      </Box>
+      </Main>
     </Box>
   );
 }
